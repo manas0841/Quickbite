30cb70f32cdd8ecc1de1747a0e0637a0ae9f1bf4
karmanveer <karman0068@gmail.com>
2025-04-22 19:49:17 +0530

    3nd commit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,8 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
     const navigate = useNavigate(); 
-    const url = "http://localhost:4000";
+    const url = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000";
+    
 
     const redirectToLogin = () => {
         navigate("/login");
